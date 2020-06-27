120f0e9feb98ba862119b29476a9e37c5abb2450
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-06-27 20:47:05 +0800

    Completed TP Quiz page

--- TPOpenHouseMobile/TPQuiz.xaml.cs
+++ TPOpenHouseMobile/TPQuiz.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,10 +16,112 @@ namespace TPOpenHouseMobile
     public partial class TPQuiz : ContentPage
     {
         User _user;
+        List<Question> _questionSet;
+        int numberOfQuestion = 0;
+        int questionIndex = 0;
         public TPQuiz(User user)
         {
             InitializeComponent();
             _user = user;
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await GetQuestions();
+        }
+
+        private async Task GetQuestions()
+        {
+            var client = new WebApi();
+            var response = await client.Post($"Questions/GetCategoryQuestions?category=TP", "");
+            _questionSet = JsonConvert.DeserializeObject<List<Question>>(response);
+            numberOfQuestion = _questionSet.Count();
+            PopulateQuestion();
+        }
+
+        private async void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            var client = new WebApi();
+            var userResponse = new UserResponse() { userIDFK = _user.userID, questionIDFK = _questionSet[questionIndex].ID };
+            if (rbAnswerOne.IsChecked)
+            {
+                userResponse.userAnswer = lblAnswerOne.Text;
+            }
+            else if (rbAnswerTwo.IsChecked)
+            {
+                userResponse.userAnswer = lblAnswerTwo.Text;
+            }
+            else if (rbAnswerThree.IsChecked)
+            {
+                userResponse.userAnswer = lblAnswerThree.Text;
+            }
+            else if (rbAnswerFour.IsChecked)
+            {
+                userResponse.userAnswer = lblAnswerFour.Text;
+            }
+
+            if (userResponse.userAnswer == _questionSet[questionIndex].Correct)
+            {
+                userResponse.isCorrect = true;
+            }
+            else
+            {
+                userResponse.isCorrect = false;
+            }
+            var JsonData = JsonConvert.SerializeObject(userResponse);
+            var response = await client.Post("UserResponses/Create", JsonData);
+            if (response == "\"Congrats! Your answer was correct!\"")
+            {
+                await DisplayAlert("Feedback", "Congrats! Your answer was correct!", "Ok");
+                if (questionIndex == numberOfQuestion - 1)
+                {
+                    await DisplayAlert("End of quiz", "You have completed the last question! Redirecting you back to Quiz Menu!", "Ok");
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    questionIndex += 1;
+                    ClearSelections();
+                    PopulateQuestion();
+                }
+            }
+            else if (response == "\"Your answer is incorrect!\"")
+            {
+                await DisplayAlert("Feedback", $"Your answer is incorrect! The correct answer is: {_questionSet[questionIndex].Correct}", "Ok");
+                if (questionIndex == numberOfQuestion - 1)
+                {
+                    await DisplayAlert("End of quiz", "You have completed the last question! Redirecting you back to Quiz Menu!", "Ok");
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    questionIndex += 1;
+                    ClearSelections();
+                    PopulateQuestion();
+                }
+            }
+            else if (response == "\"Unable to submit response!Please try again later!\"")
+            {
+                await DisplayAlert("Feedback", "Unable to submit response! Please try again later!", "Ok");
+                await Navigation.PopAsync();
+            }
+        }
+
+        private void ClearSelections()
+        {
+            rbAnswerOne.IsChecked = false;
+            rbAnswerTwo.IsChecked = false;
+            rbAnswerThree.IsChecked = false;
+            rbAnswerFour.IsChecked = false;
+        }
+
+        private void PopulateQuestion()
+        {
+            lblAnswerOne.Text = _questionSet[questionIndex].AnswerOne;
+            lblAnswerTwo.Text = _questionSet[questionIndex].AnswerTwo;
+            lblAnswerThree.Text = _questionSet[questionIndex].AnswerThree;
+            lblAnswerFour.Text = _questionSet[questionIndex].AnswerFour;
+            lblQuestion.Text = _questionSet[questionIndex].questionString;
         }
     }
 }
